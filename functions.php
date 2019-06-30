9c9439693f984fe7d57cc5984d5073c1a5817f36
tgarriz <tgarriz@gmail.com>
2019-06-30 08:55:57 -0300

    arreglado los registros en debe/haber en 0

--- functions.php
+++ functions.php
@@ -22,6 +22,16 @@ if(isset($_POST["Import"])){
 	            window.location = \"index.php\"
       	        </script>";    
 			  } else {
+				/*	$result2 = pg_query($con,"update asiento01 set debe = '' where debe = '0,00'");
+					if(!isset($result2)){
+						echo "error en result2";
+						exit();
+					}
+					$result3 = pg_query($con,"update asiento01 set haber = '' where debe = '0,00'");
+					if(!isset($result3)){
+						echo "error en result3";
+						exit();
+					}*/
       	        echo "<script type=\"text/javascript\">
               	alert(\"CSV Importado.\");
 	            window.location = \"index.php\"
@@ -76,7 +86,7 @@ function crea_datos($con,$asiento){
 				rpad(creaNumero(debe),15,' ') ||
 				regexp_replace(fecha, '/', '', 'g') as campo
 			from asiento01
-			where debe != '' AND nro_asiento = '$asiento' 
+			where debe != '' AND debe != '0,00' AND nro_asiento = '$asiento' 
 			union
 			select  rpad(nro_asiento,6,' ') ||
 				rpad(replace(replace(cod_cta,'.',''),'/',''),15,' ') ||
@@ -88,7 +98,7 @@ function crea_datos($con,$asiento){
 				rpad(creaNumero(haber),15,' ') ||
 				regexp_replace(fecha, '/', '', 'g') as campo
 			from asiento01
-			where haber != '' AND nro_asiento = '$asiento'";
+			where haber != '' AND haber != '0,00' AND nro_asiento = '$asiento'";
 
 	$result = pg_query($con,$sql);
 	$file = fopen("files/asiento-".$asiento."2.imp","w");
